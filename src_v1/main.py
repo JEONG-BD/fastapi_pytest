c7c54b762175bca63a26c0c3765091afcda5f6d7
JEONG-BD <dev.byeongdu@gmail.com>
2025-02-26 09:03:49 +0900

    log

--- src_v1/main.py
+++ src_v1/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+import logging.config
+#logging.basicConfig(level=logging.DEBUG)
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get('/')
 def root():
+    try:
+        raise Exception('Simulated debug error occurred')
+    except Exception as e:
+        logging.debug(f"An exception occurred")
+        logging.debug(f"Exception traceback : ", exc_info=True)
+
     return {"root": "root"}
 
 
